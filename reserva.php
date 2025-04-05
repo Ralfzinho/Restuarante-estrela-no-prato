3195ff25964d5c07a3adfabd16788cfeb6d0cf78
Ralf da Silva <ralfsilvapf@gmail.com>
2025-04-05 10:40:20 -0300

    Ajustando os erros no PHP

--- reserva.php
+++ reserva.php
@@ -2,14 +2,15 @@
 $host = "sql301.infinityfree.com";
 $user = "if0_38679193";
 $password = "sUEBKsU9Uy6ahp";
-$dbname = "if0_38679193_XXX";
+$dbname = "if0_38679193_db_reserva"; // substitua pelo nome correto do seu banco
 
-
+// CORREÇÃO: variável estava errada ($coon -> $conn)
 $conn = new mysqli($host, $user, $password, $dbname);
-if ($coon->connect_error) {
-    die("Erro na conexção: " . $conn->connect_error);
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
 }
 
+// Recebe os dados do formulário
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $telefone = $_POST['telefone'];
@@ -22,14 +23,15 @@ $alergia = $_POST['alergia'];
 $extras = $_POST['extras'];
 $confirmacao = isset($_POST['confirmacao']) ? implode(", ", $_POST['confirmacao']) : "";
 
-
-$sql = "INSERT INTO reservas
+// Cria a SQL
+$sql = "INSERT INTO reservas 
         (nome, email, telefone, data_reserva, hora, convidados, tipo, mesa, alergia, extras, confirmacao)
-        VALUES
+        VALUES 
         ('$nome', '$email', '$telefone', '$data', '$hora', '$convidados', '$tipo', '$mesa', '$alergia', '$extras', '$confirmacao')";
 
-if ($conn->($sql) === TRUE) {
-    echo "Reserva Realizada com Sucesso!";
+// CORREÇÃO: chamada do método estava errada -> $conn->($sql) -> $conn->query($sql)
+if ($conn->query($sql) === TRUE) {
+    echo "Reserva realizada com sucesso!";
 } else {
     echo "Erro ao enviar reserva: " . $conn->error;
 }
