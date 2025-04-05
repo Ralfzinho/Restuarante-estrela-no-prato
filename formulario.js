2a40a0f35d5a6b4d12723ac1fd989d613bf00457
Ralf da Silva <125992274+Ralfzinho@users.noreply.github.com>
2025-04-05 20:29:48 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -15,26 +15,3 @@ form.addEventListener("submit", event => {
     // (usado pelo Bootstrap para mostrar mensagens de validação)
     form.classList.add("was-validated");
 }, false);
-
-form.addEventListener('submit', function (e) {
-  // Combina checkboxes da mesa
-  const mesa = [...form.querySelectorAll('input[name="mesa[]"]:checked')]
-                .map(el => el.value).join(', ');
-  // Combina checkboxes da confirmação
-  const confirmacao = [...form.querySelectorAll('input[name="confirmacao[]"]:checked')]
-                       .map(el => el.value).join(', ');
-
-  // Cria inputs escondidos que serão enviados no lugar dos arrays
-  const mesaInput = document.createElement('input');
-  mesaInput.type = 'hidden';
-  mesaInput.name = 'data[mesa]';
-  mesaInput.value = mesa;
-
-  const confirmacaoInput = document.createElement('input');
-  confirmacaoInput.type = 'hidden';
-  confirmacaoInput.name = 'data[confirmacao]';
-  confirmacaoInput.value = confirmacao;
-
-  form.appendChild(mesaInput);
-  form.appendChild(confirmacaoInput);
-});
