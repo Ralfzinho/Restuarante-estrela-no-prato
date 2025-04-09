ceaeb3fb1fd6a669328769a62b96ccde9600eb3d
Ralf da Silva <ralfsilvapf@gmail.com>
2025-04-09 16:54:25 -0300

    ajustando o css
    removendo o que não é mais usado

--- js/tema.js
+++ js/tema.js
@@ -2,4 +2,14 @@ const chk = document.getElementById('chk')
 
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark')
+
+    const navbar = document.querySelector('.navbar')
+
+    if (document.body.classList.contains('dark')) {
+        navbar.classList.remove('navbar-light')
+        navbar.classList.add('navbar-dark', 'bg-dark')
+    } else {
+        navbar.classList.remove('navbar-dark', 'bg-dark')
+        navbar.classList.add('navbar-light')
+    }
 })
